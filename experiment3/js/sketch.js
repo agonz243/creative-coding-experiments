8cffb32902f4d21899514e61f499f37d3d9ee2dc
Aaron Gonzales <agonz243@ucsc.edu>
2023-01-29 13:00:40 -0800

    Started exp 3

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,15 +1,33 @@
-let rules = {
-	"A": "AB",
-	"B": "A"
-}
-
-let axiom = "X"
+let angle = 0;
 
 function setup() {
-	createCanvas(800, 800)
-	background(0, 0, 0)
+	createCanvas(600, 600);
 }
 
 function draw() {
 
+	clear()
+
+	// Create grid of circles
+	for (let x = 0; x < width; x += 25) {
+		for (let y = 0; y < height; y += 25) {
+			ellipse(x, y, 10, 10);
+		}
+	}
+
+
+
+
+
+
+
+	/*
+	clear()
+	let radius = 20;
+	let circleX = mouseX + radius * cos(angle);
+	let circleY = mouseY + radius * sin(angle);
+
+	ellipse(circleX, circleY, 10, 10);
+	angle += 0.05;
+	*/
 }
