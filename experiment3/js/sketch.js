278324569a9bc30cf7f5e329ef02fc00d8e1c5e8
Aaron Gonzales <agonz243@ucsc.edu>
2023-01-29 16:13:28 -0800

    Reversed render effect and added lerp to orbit

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -3,11 +3,15 @@ let angle;
 let radius;
 let circleX;
 let circleY;
+let offsetX;
+let offsetY
 
 function setup() {
 	createCanvas(600, 600);
 	angle = 0;
-	radius = 50;
+	radius = 100;
+	offsetX = 0;
+	offsetY = 0;
 }
 
 function draw() {
@@ -19,9 +23,9 @@ function draw() {
 		for (let y = 0; y < height; y += 25) {
 
 			// If a circle is within range, make it rotate around cursor
-			if (distance(mouseX, mouseY, x, y) <= radius) {
-				circleX = mouseX + radius * cos(angle + x + y);
-				circleY = mouseY + radius * sin(angle + x + y);
+			if (distance(mouseX, mouseY, x, y) >= radius) {
+				circleX = mouseX + radius * cos(angle + x + y + offsetX);
+				circleY = mouseY + radius * sin(angle + x + y + offsetY);
 				ellipse(circleX, circleY, 10, 10);
 
 			} else { // Draw it within the grid
@@ -32,6 +36,11 @@ function draw() {
 
 	// Constantly increment angle for rotation effect
 	angle += 0.05;
+
+	if (mouseIsPressed) {
+		offsetX += 0.02;
+		offsetY -= 0.02;
+	}
 }
 
 // Function for calculating the distance between two points
@@ -39,3 +48,5 @@ function draw() {
 function distance(x1, y1, x2, y2) {
 	return sqrt( sq((x2 - x1)) + sq((y2 - y1)) );
 }
+
+
