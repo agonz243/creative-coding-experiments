88143a900983db203d00cfca1e1007b6bb786e49
Aaron Gonzales <agonz243@ucsc.edu>
2023-02-20 09:34:58 -0800

    Added 3d typography effect

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,8 +1,15 @@
-// sketch.js - purpose and description here
+// sketch.js - A "Worry Simulator" that I created to visually convey the pressure of
+//             modern day anxieties. Text is generated using Tracery.js
+//
+// Forked from "3D Typography Study" by Zoulei on openprocessing.org
+// https://openprocessing.org/sketch/691190
+//
 // Author: Aaron Gonzales
-// Date: 02/21/2023
+// Last Edited: 02/21/2023
 
-let generatedText;
+let str;
+let str_arr = [];
+let font;
 
 let grammar = 
 {
@@ -14,10 +21,19 @@ let grammar =
     "trait": ["stupid", "weird", "boring", "annoying"]
 }
 
+function preload() {
+    font = loadFont("./fonts/TypewriterInked.ttf");
+}
+
 function setup() {
-    let canvas = createCanvas(800, 800);
-    background(0, 0, 0);
+    let canvas = createCanvas(1500, 800, WEBGL);
     canvas.parent("canvas-container");
+
+    textAlign(CENTER, CENTER);
+    textFont(font);
+    textSize(100);
+    fill(255,255,255);
+    //translate(0, 0, 50);
 
     // Resize canvas if the page is resized
     $(window).resize(function() {
@@ -25,16 +41,25 @@ function setup() {
         resizeCanvas(canvas.width, canvas.height);
     });
 
-    textSize(20);
-    fill(255);
-    textAlign(CENTER, CENTER);
-    generatedText = loadGrammar();
+    // Generate strings and add them to an array as Type objects
+    for (let i = 0; i < 10; i++) {
+      let x = random(-width / 2, width / 2);
+      let y = random(-height / 2, height / 2);
+      let z = random(-width*5, width/2);
+      str_arr.push(new Type(loadGrammar(), x, y, z));
+    }
 
 }
 
 function draw() {
     background(0, 0, 0);
-    text(generatedText, width / 2, height / 2);
+
+    // For evert string in the array, display it on screen with animation
+	//orbitControl();
+    for (let i = 0; i < str_arr.length; i++) {
+        str_arr[i].update();
+        str_arr[i].display();
+    }
 }
 
 function loadGrammar() {
@@ -42,7 +67,26 @@ function loadGrammar() {
     return grammarObj.flatten('#origin#');
 }
 
-function mouseClicked() {
-    console.log("Generated Text")
-    generatedText = loadGrammar();
-}
+
+class Type {
+    constructor(_str, _x, _y, _z) {
+      this.str = _str;
+      this.x = _x;
+      this.y = _y;
+      this.z = _z;
+    }
+  
+    update() {
+      this.z += 30;
+      if(this.z > width/8){
+          this.z = -width*5;
+      }
+    }
+  
+    display() {
+      push();
+      translate(this.x, this.y, this.z);
+      text(this.str, 0, 0);
+      pop();
+    }
+  }
