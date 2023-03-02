4eba5a312f9ab00b7ebf506ce45db61c34c281c1
Aaron Gonzales <agonz243@ucsc.edu>
2023-03-01 16:22:27 -0800

    Done with most of it

--- experiment7/js/sketch.js
+++ experiment7/js/sketch.js
@@ -12,6 +12,19 @@ let canClick = true;
 let shootings;
 let t5Text;
 
+let person3;
+let person6;
+let person9;
+let graphic1;
+let graphic2;
+let graphic3;
+
+function preload() {
+  graphic_b = loadImage("./assets/graphic_b.png");
+  graphic_h = loadImage("./assets/graphic_h.png");
+  graphic_w = loadImage("./assets/graphic_w.png");
+}
+
 function setup() {
   let canvas = createCanvas(800, 600);
   canvas.parent("canvas-container");
@@ -30,8 +43,19 @@ function setup() {
   textArr = ["Hello there, how are you?", "I'd Like to show you something", 
   "Indulge me, if you will. As I give you a glimpse...", "...of life as a Black or Hispanic American",
   "Since, 2015 The Washington Post has tracked " + shootings + " fatal police shootings", 
-  "Despite making up only 32.5 % of the population...", "45%", "This is, no doubt, disproportionate"]
+  "Despite making up only 32.5 % of the population...", "45%", "This is, no doubt, disproportionate",
+  "White Americans are shot at a rate of 2.3 per million of the population, per year...",
+  "Hispanic Americans, at a rate of 2.6 per million...", 
+  "and Black Americans at a rate of 5.9 per million",
+  "We must refute the idea that Black or Hispanic Americans commit more crime",
+  "Instead, we must accept that deeply rooted issues exist within our Police Forces",
+  "Be part of the solution...", "Follow the facts...", "and think for yourself", "Thank you."]
   currText = new FadeText(textArr[t], midX, midY, 32)
+
+  // Create fading graphics
+  graphic1 = new FadeImage(graphic_w, 200, 275, 30, 170);
+  graphic2 = new FadeImage(graphic_h, 400, 255, 30, 200);
+  graphic3 = new FadeImage(graphic_b, 600, 50, 30, 410);
 }
 
 function draw() {
@@ -82,6 +106,48 @@ function draw() {
     currText.s = 32;
     currText.y = midY;
   }
+
+  if (t == 8 || t == 9 || t == 10) {
+    currText.y = midY + 200;
+    
+    graphic1.display();
+    if (graphic1.opacity > 0) {
+      canClick = false;
+    } else {
+      canClick = true;
+    }
+  }
+
+  if (t == 9 || t == 10) {
+    graphic2.display();
+    if (graphic2.opacity > 0) {
+      canClick = false;
+    } else {
+      canClick = true;
+    }
+  }
+
+  if (t == 10) {
+    graphic3.display();
+    if (graphic3.opacity > 0) {
+      canClick = false;
+    } else {
+      canClick = true;
+    }
+  }
+
+  if (t == 11) {
+    graphic1.display();
+    graphic2.display();
+    graphic3.display();
+    graphic1.faded = true;
+    graphic2.faded = true;
+    graphic3.faded = true;
+
+    if (graphic1.opacity >= 300 && currText.y > midY) {
+      currText.y -= 1;
+    }
+  }
 }
 
 function mouseClicked() {
@@ -117,6 +183,47 @@ class FadeText {
   fadeIn() {
     if (this.opacity < 200) {
       this.opacity += 2;
+    }
+  }
+}
+
+class FadeImage {
+  constructor(_img, _x, _y, _width, _height) {
+    this.img = _img;
+    this.x = _x;
+    this.y = _y;
+    this.w = _width;
+    this.h = _height;
+    this.opacity = 300;
+    this.faded = false; // Whether or not the image should be faded
+  }
+
+  display() {
+    if (this.opacity < 300) {
+      push()
+      translate(this.x, this.y);
+      image(this.img, 0, 0, this.w, this.h);
+      fill(0, 0, 0, this.opacity);
+      rect(0, 0, this.w * 2, this.h * 2);
+      pop();
+    }
+
+    if (this.faded) {
+      this.fadeOut();
+    } else {
+      this.fadeIn();
+    }
+  }
+
+  fadeIn() {
+    if (this.opacity > 0) {
+      this.opacity -= 3;
+    }
+  }
+
+  fadeOut() {
+    if (this.opacity < 300) {
+      this.opacity += 3;
     }
   }
 }
